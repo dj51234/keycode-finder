45b6c330fdf38c06abd3b3fb6c7559b9b04e9706
dj51234 <dj51234@yahoo.com>
2021-12-16 00:50:53 -0500

    initial push

---
+++ main.js
@@ -0,0 +1,27 @@
+const code = document.querySelector(".code");
+const $key = document.querySelector(".key");
+const which = document.querySelector(".which");
+const getStarted = document.querySelector(".get-started");
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("keydown", (event) => {
+  getStarted.classList.add("hide");
+
+  boxes.forEach((box) => {
+    box.classList.add("show");
+  });
+
+  if (event.code === "Space") {
+    $key.textContent = "spacebar";
+  } else if (event.key === "Tab") {
+    event.preventDefault();
+    $key.textContent = event.key;
+  } else {
+    $key.textContent = event.key;
+  }
+
+  which.textContent = event.which;
+  code.textContent = event.code;
+
+  console.log(event);
+});
